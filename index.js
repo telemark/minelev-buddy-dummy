8b53c410e77b9ba52103057fe16f81b3d1eb4e09
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-10-02 16:18:33 +0200

    Updates Node. Updates dependencies

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const readFileSync = require('fs').readFileSync
-const marked = require('marked')
+const md = require('markdown-it')()
 const { send } = require('micro')
 const { parse } = require('url')
 const actionHandler = require('./lib/action-handler')
@@ -15,8 +15,7 @@ module.exports = async (request, response) => {
     send(response, 200, { ping: 'pong', 'name': pkg.name, version: pkg.version })
   } else if (pathname === '/') {
     const readme = readFileSync('./README.md', 'utf-8')
-    const html = marked(readme)
-    send(response, 200, html)
+    send(response, 200, md.render(readme))
   } else {
     send(response, 200, actionHandler(request))
   }
